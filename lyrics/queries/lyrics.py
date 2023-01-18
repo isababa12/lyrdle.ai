ba44c159887140e3a229a5c2832e30107bb36b78
Stephanie Hestilow <stephaniehestilow@gmail.com>
2023-01-18 21:42:14 +0000

    Auth for Lyrics Backend

--- lyrics/queries/lyrics.py
+++ lyrics/queries/lyrics.py
@@ -5,12 +5,9 @@ from queries.pool import pool
 
 
 class LyricsIn(BaseModel):
-    # user_id: int
     user_input: str
     artist_name: str
     song_name: str
-    # ai_prompt: str
-    # user_output: str
 
 class LyricsOut(BaseModel):
     id: int
@@ -20,11 +17,20 @@ class LyricsOut(BaseModel):
     song_name: str
     ai_prompt: str
     user_output: str
-    posted: bool
-    # created_at: datetime
-    # posted_at: Optional[datetime]
+    created_at: Optional[datetime]
+    posted: Optional[bool]
+    posted_at: Optional[datetime]
 
-class LyricsPosted(BaseModel):
+class LyricsCreateOut(BaseModel):
+    id: int
+    user_id: int
+    user_input: str
+    artist_name: str
+    song_name: str
+    ai_prompt: str
+    user_output: str
+
+class LyricsStatus(BaseModel):
     posted:bool
 
 class Error(BaseModel):
@@ -33,39 +39,26 @@ class Error(BaseModel):
 
 class LyricsQueries:
 
-    def update_posted(self, lyrics_id: int, posted: bool) -> LyricsPosted:
-        try:
-            with pool.connection() as conn:
-                with conn.cursor() as cur:
-                    cur.execute(
-                        """
-                        UPDATE lyrics
-                        SET posted = %s
-                        WHERE id = %s
-                        """,
-                        [
-                            posted,
-                            lyrics_id
-                        ]
-                    )
-                return LyricsPosted(posted=posted)
-        except Exception as e:
-            print(e)
-            return {"message": "Unable to post Lyrics"}
-
+    # Create new lyrics
     def create(self,
         input: LyricsIn,
         user_id: int,
         ai_prompt: str,
         user_output: str,
-    ) -> LyricsOut:
+    ) -> LyricsCreateOut:
         try:
             with pool.connection() as conn:
                 with conn.cursor() as cur:
                     result = cur.execute(
                         """
-                        INSERT INTO lyrics
-                            (user_id, user_input,  artist_name, song_name, ai_prompt, user_output)
+                        INSERT INTO lyrics(
+                            user_id
+                            , user_input
+                            , artist_name
+                            , song_name
+                            , ai_prompt
+                            , user_output
+                        )
                         VALUES
                             (%s, %s, %s, %s, %s, %s)
                         RETURNING id;
@@ -81,30 +74,57 @@ class LyricsQueries:
                     )
                     id = result.fetchone()[0]
                     old_data = input.dict()
-                    return LyricsOut(id=id, user_id=user_id, ai_prompt=ai_prompt, user_output=user_output, **old_data)
+                    return LyricsCreateOut(
+                        id=id,
+                        user_id=user_id,
+                        ai_prompt=ai_prompt,
+                        user_output=user_output,
+                        **old_data
+                    )
         except Exception:
             return {"message": "create did not work"}
 
-
-    def get_all(self) -> Union[List[LyricsOut], Error]:
+    # Get all lyrics
+    def get_all(self, user_id: int = None) -> Union[List[LyricsOut], Error]:
         try:
             with pool.connection() as conn:
                 with conn.cursor() as db:
-                    result = db.execute(
-                        """
-                        SELECT id
-                            , user_id
-                            , user_input
-                            , artist_name
-                            , song_name
-                            , ai_prompt
-                            , user_output
-                            , posted
-                            , created_at
-                        FROM lyrics
-                        ORDER BY created_at DESC;
-                        """
-                    )
+                    if user_id is None:
+                        result = db.execute(
+                            """
+                            SELECT id
+                                , user_id
+                                , user_input
+                                , artist_name
+                                , song_name
+                                , ai_prompt
+                                , user_output
+                                , created_at
+                                , posted
+                                , posted_at
+                            FROM lyrics
+                            ORDER BY created_at DESC;
+                            """
+                        )
+                    else:
+                        result = db.execute(
+                            """
+                            SELECT id
+                                , user_id
+                                , user_input
+                                , artist_name
+                                , song_name
+                                , ai_prompt
+                                , user_output
+                                , created_at
+                                , posted
+                                , posted_at
+                            FROM lyrics
+                            WHERE user_id = %s
+                            ORDER BY created_at DESC;
+                            """,
+                            [user_id]
+                        )
                     return [
                         self.record_to_lyrics_out(record)
                         for record in result
@@ -114,7 +134,7 @@ class LyricsQueries:
             return {"message": "Could not get all lyrics"}
 
 
-    # Get one (by lyrics id)
+    # Get one lyrics (by lyrics id)
     def get_one(self, lyrics_id: int) -> Optional[LyricsOut]:
         try:
             with pool.connection() as conn:
@@ -128,8 +148,9 @@ class LyricsQueries:
                             , song_name
                             , ai_prompt
                             , user_output
-                            , posted
                             , created_at
+                            , posted
+                            , posted_at
                         FROM lyrics
                         WHERE id = %s
                         """,
@@ -143,20 +164,39 @@ class LyricsQueries:
             print(e)
             return {"message": "Could not get that lyrics item"}
 
-    # def update
+    # Change "posted" status for one lyrics
+    def update_status(self, lyrics_id: int, posted: bool) -> LyricsStatus:
+        try:
+            with pool.connection() as conn:
+                with conn.cursor() as cur:
+                    cur.execute(
+                        """
+                        UPDATE lyrics
+                        SET posted = %s
+                        WHERE id = %s
+                        """,
+                        [
+                            posted,
+                            lyrics_id
+                        ]
+                    )
+                return LyricsStatus(posted=posted)
+        except Exception as e:
+            print(e)
+            return {"message": "Unable to post Lyrics"}
 
-    # def delete
 
-    # helper function - record to lyrics out
     def record_to_lyrics_out(self, record):
+        # print("Record: ", record)
         return LyricsOut(
             id=record[0],
             user_id=record[1],
             user_input=record[2],
-            ai_prompt=record[3],
-            artist_name=record[4],
-            song_name=record[5],
+            artist_name=record[3],
+            song_name=record[4],
+            ai_prompt=record[5],
             user_output=record[6],
-            posted=record[7],
-            created_at=record[8]
+            created_at=record[7],
+            posted=record[8],
+            posted_at=record[9]
         )
