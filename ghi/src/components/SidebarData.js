4e993855607db644fb51115d38549be48e183eda
Harry Zeng <harryzeng1998@gmail.com>
2023-01-19 01:53:24 +0000

    Harry 118 branch

---
+++ ghi/src/components/SidebarData.js
@@ -0,0 +1,43 @@
+import React from "react";
+// import * as FaIcons from "react-icons/fa";
+import * as AiIcons from "react-icons/ai";
+import * as IoIcons from "react-icons/io";
+
+export const SidebarData = [
+  {
+    title: "Home",
+    path: "/",
+    icon: <AiIcons.AiFillHome />,
+    cName: "nav-text",
+  },
+  {
+    title: "Create",
+    path: "/create",
+    icon: <IoIcons.IoIosCreate />,
+    cName: "nav-text",
+  },
+  {
+    title: "Profile",
+    path: "/profile",
+    icon: <IoIcons.IoIosAlbums />,
+    cName: "nav-text",
+  },
+  {
+    title: "Setting",
+    path: "/setting",
+    icon: <IoIcons.IoIosSettings />,
+    cName: "nav-text",
+  },
+  {
+    title: "Login",
+    path: "/login",
+    icon: <IoIcons.IoIosLogIn />,
+    cName: "nav-text",
+  },
+//   {
+//     title: "logout",
+//     path: "/logout",
+//     icon: <IoIcons.IoIosLogOut />,
+//     cName: "nav-text",
+//   },
+];
---
+++ ghi/src/pages/Home.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Home() {
+  return (
+    <div class='home'>
+        <h1>Home</h1>
+    </div>
+  )
+}
+
+export default Home
