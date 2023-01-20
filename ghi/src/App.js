952fa469b8777e8978bfc02b8ea67c12b651eb98
stephanie hestilow <stephaniehestilow@gmail.com>
2023-01-19 17:37:38 -0800

    updated posted status for lyrics, working on homepage on frontend

--- ghi/src/App.js
+++ ghi/src/App.js
@@ -1,9 +1,9 @@
-import { useEffect, useState } from 'react';
+// import { useEffect, useState } from 'react';
 // import Construct from './Construct.js'
 // import ErrorNotification from './ErrorNotification';
 import './App.css';
 import Navbar from './components/Navbar.js';
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, BrowserRouter } from "react-router-dom";
 import Home from "./pages/Home";
 import Profile from "./pages/Profile";
 import Create from "./pages/Create";
@@ -12,35 +12,35 @@ function App() {
   // const [launch_info, setLaunchInfo] = useState([]);
   // const [error, setError] = useState(null);
 
-  useEffect(() => {
-    async function getData() {
-      let url = `${process.env.REACT_APP_SAMPLE_SERVICE_API_HOST}/api/launch-details`;
-      console.log('fastapi url: ', url);
-      let response = await fetch(url);
-      console.log("------- hello? -------");
-      let data = await response.json();
+  // useEffect(() => {
+  //   async function getData() {
+  //     let url = `${process.env.REACT_APP_SAMPLE_SERVICE_API_HOST}/api/launch-details`;
+  //     console.log('fastapi url: ', url);
+  //     let response = await fetch(url);
+  //     console.log("------- hello? -------");
+  //     let data = await response.json();
 
-      if (response.ok) {
-        console.log("got launch data!");
-        setLaunchInfo(data.launch_details);
-      } else {
-        console.log("drat! something happened");
-        setError(data.message);
-      }
-    }
-    getData();
-  }, [])
+  //     if (response.ok) {
+  //       console.log("got launch data!");
+  //       setLaunchInfo(data.launch_details);
+  //     } else {
+  //       console.log("drat! something happened");
+  //       setError(data.message);
+  //     }
+  //   }
+  //   getData();
+  // }, [])
 
 
    return (
-     <Router>
-       <Navbar />
+    <BrowserRouter>
+      <Navbar />
        <Routes>
-         <Route path="/" exact component={Home} />
-         <Route path="/create" component={Create} />
-         <Route path="/profile" component={Profile} />
+         <Route path="/" element={<Home />} />
+         <Route path="/create" element= {<Create />} />
+         <Route path="/profile" element= {<Profile />} />
        </Routes>
-     </Router>
+    </BrowserRouter>
    );
 }
 
